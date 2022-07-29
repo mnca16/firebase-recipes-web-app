49804e95bbbd08053c63dda2f7300009f8ecabf4
Monica Delgado <mnca.delgado@gmail.com>
2022-07-29 12:06:22 -1100

    added create recipe form and validate its values with firebase

---
+++ src/FirebaseFirestoreService.js
@@ -0,0 +1,17 @@
+import firebase from "./FirebaseConfig"
+
+const firestore = firebase.firestore()
+
+const createDocument = (collection, document) => {
+  return firestore.collection(collection).add(document)
+}
+
+const readDocuments = (collection) => {
+  return firestore.collection(collection).add(document)
+}
+const FirebaseFirestoreService = {
+  createDocument,
+  readDocuments,
+}
+
+export default FirebaseFirestoreService
