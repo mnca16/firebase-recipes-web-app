1d8281039f5251a63acb2605f81cb9e038bb5835
Monica Delgado <mnca.delgado@gmail.com>
2022-07-31 15:46:16 -1100

    fetched data with firestore

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import FirebaseAuthService from "./FirebaseAuthService"
 import LoginForm from "./Components/LoginForm"
 import AddEditRecipeForm from "./Components/AddEditRecipeForm"
@@ -8,9 +8,67 @@ import FirebaseFirestoreService from "./FirebaseFirestoreService"
 function App() {
   //Initialized user to null
   const [user, setUser] = useState(null)
+  //render stored recipes in firebase data base
+  const [recipes, setRecipes] = useState([])
+
+  //call the data when the user states changes
+  useEffect(() => {
+    fetchRecipes()
+      .then((fetchedRecipes) => {
+        setRecipes(fetchedRecipes)
+      })
+      .catch((error) => {
+        console.error(error.message)
+        throw error
+      })
+  }, [user])
 
   //Detects when auth changes occurs and pass in the user being used
   FirebaseAuthService.subscribeToAuthChanges(setUser)
+
+  //Fetch recipes from firebase using firebase methos
+  async function fetchRecipes() {
+    let fetchedRecipes = []
+
+    try {
+      const response = await FirebaseFirestoreService.readDocuments("recipes")
+      console.log(response)
+      console.log("Is this working???????????")
+      const newRecipes = response.docs.map((recipeDoc) => {
+        // console.log("inside newRecipes", recipeDoc.id)
+        // console.log("inside newRecipes DATA", recipeDoc.data())
+        const id = recipeDoc.id
+        const data = recipeDoc.data()
+        // console.log("inside newRecipes DATA variable", data)
+        console.log(
+          "inside of newRecipes DATA data.publishedData",
+          (data.publishDate = new Date(data.publishedDate.seconds * 1000))
+        )
+        data.publishedDate = new Date(data.publishedDate.seconds * 1000)
+        //console.log("inside newRecipes DATA variable # 2", data)
+        return { ...data, id }
+      })
+      console.log("Is this working???????????")
+      console.log("NEW RECIPES", newRecipes)
+
+      fetchedRecipes = [...newRecipes]
+    } catch (error) {
+      console.error(error.message)
+      throw error
+    }
+    return fetchedRecipes
+  }
+
+  //handle recipes on demand and updates the state
+  async function handleFetchRecipes() {
+    try {
+      const fetchedRecipes = await fetchRecipes()
+      setRecipes(fetchedRecipes)
+    } catch (error) {
+      console.error(error.message)
+      throw error
+    }
+  }
 
   //handle when AddEditRecipeForm has create a new recipe
   async function handleAddRecipe(newRecipe) {
@@ -20,7 +78,10 @@ function App() {
         newRecipe
       )
 
-      //TODO: fetch new recipes from firestore
+      //TODO: fetch new recipes from firestore, handle fetche recipes is going
+      //to fire everytime a recipe is added
+      handleFetchRecipes()
+
       alert(`succesfully created a recipe with an ID = ${response.id}`)
     } catch (error) {
       alert(error.message)
@@ -34,6 +95,27 @@ function App() {
         <LoginForm existingUser={user}></LoginForm>
       </div>
       <div className="main">
+        <div className="center">
+          <div className="recipe-list-box">
+            {recipes && recipes.length > 0 ? (
+              <div className="recipe-list">
+                {recipes.map((recipe) => {
+                  return (
+                    <div className="recipecard" key={recipe.key}>
+                      <div className="recipe-name">{recipe.name}</div>
+                      <div className="recipe-field">
+                        Category: {recipe.category}
+                      </div>
+                      <div className="class-field">
+                        Publish Date: {recipe.publishDate.toSring()}
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
+            ) : null}
+          </div>
+        </div>
         {user ? (
           <AddEditRecipeForm
             handleAddRecipe={handleAddRecipe}
